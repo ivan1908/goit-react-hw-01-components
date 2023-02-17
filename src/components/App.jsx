bb7e5fb60c0e30af5f356dbb5d7f8f028d084f51
Ivan Hema <hacker19088@gmail.com>
2023-02-17 11:28:07 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import Profile from './Profile/Profile.jsx';
+import user from '../base/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      < Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+    </>
   );
 };
