11a7413c1c6ea48adc96feef995bb39410ffccf2
Ivan Hema <hacker19088@gmail.com>
2023-02-17 11:50:35 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import Profile from './Profile/Profile.jsx';
 import user from '../base/user.json';
+import Statistics from './Statistics/Statistics.jsx';
+import data from '../base/data.json';
+
+import friends from '../base/friends.json';
+
+import transactions from '../base/transactions.json';
 
 export const App = () => {
   return (
     <>
       < Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+      < Statistics title="Upload stats" stats={data} />
     </>
   );
 };
